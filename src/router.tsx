1e764b23e6c0c3c75dc1bcaa568b6d9ae9db33da
perekoshik <egdstudioss@gmail.com>
2025-10-27 03:21:05 +0300

    fix: switch to HashRouter for GitHub Pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import App from './App'
 import Home from './pages/Home'
 import Item from './pages/Item'
 import Profile from './pages/Profile'
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/',
     element: <App />,
